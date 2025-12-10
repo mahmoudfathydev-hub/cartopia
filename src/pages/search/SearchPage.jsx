846d6f76db6a2b092daee809f599fea44e2b4066
Mahmoud Fathy <mahmoudfathy.dev@gmail.com>
2025-12-10 18:52:28 +0200

    Fix import casing for Products.jsx in SearchPage

--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Pagetransition from "../../components/Pagetransition";
-import Products from "../../components/slideProducts/products";
+import Products from '../../components/slideProducts/Products.jsx';
+
 import { tr } from "framer-motion/client";
 
 export default function SearchPage() {
